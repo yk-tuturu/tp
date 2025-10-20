bab2be5419b8b3e1b7e3b1bf918a52546d326c65
csrhui <121076878+csrhui@users.noreply.github.com>
2025-10-20 12:57:07 +0800

    Add Subject enum, ScoreDict and SubjectRegistry

---
+++ src/main/java/seedu/address/model/subject/ScoreDict.java
@@ -0,0 +1,63 @@
+package seedu.address.model.subject;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import seedu.address.model.person.Person;
+
+/**
+ * Represents a mapping from {@link Person} to their scores in a particular subject.
+ * Provides controlled access to add, remove, and query scores.
+ */
+public class ScoreDict {
+    private final Map<Person, Integer> scores = new HashMap<>();
+
+    /**
+     * Adds or updates the score of a person.
+     *
+     * @param person The person whose score is to be set.
+     * @param score The score to assign to the person.
+     */
+    void setScore(Person person, int score) {
+        scores.put(person, score);
+    }
+
+    /**
+     * Removes a person from this score dictionary.
+     *
+     * @param person The person to remove.
+     */
+    void removePerson(Person person) {
+        scores.remove(person);
+    }
+
+    /**
+     * Retrieves the score of the given person.
+     *
+     * @param person The person whose score is to be retrieved.
+     * @return An {@link Optional} containing the score if present, otherwise empty.
+     */
+    Optional<Integer> getScore(Person person) {
+        return Optional.ofNullable(scores.get(person));
+    }
+
+    /**
+     * Checks if the given person has a score in this dictionary.
+     *
+     * @param person The person to check.
+     * @return {@code true} if the person has a score, {@code false} otherwise.
+     */
+    boolean contains(Person person) {
+        return scores.containsKey(person);
+    }
+
+    /**
+     * Returns an unmodifiable view of all scores in this dictionary.
+     *
+     * @return A map from {@link Person} to their scores.
+     */
+    public Map<Person, Integer> getAllScores() {
+        return Collections.unmodifiableMap(scores);
+    }
+}
