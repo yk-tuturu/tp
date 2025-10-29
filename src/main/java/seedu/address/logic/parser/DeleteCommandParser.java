03ac35b6a0c7bbad1bcfe3e435697f808271886c
wendy <wendytangxd@gmail.com>
2025-10-29 11:43:18 +0800

    Bring back the full delete command usage instructions

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -15,6 +17,9 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public DeleteCommand parse(String args) throws ParseException {
+        if (args.trim().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+        }
         Index[] indices = ParserUtil.parseIndexArray(args);
         return new DeleteCommand(indices);
     }
