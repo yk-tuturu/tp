9f2c65b93ef7a1d8858d2d28e61ebb1ab0bfdf4d
wendy <wendytangxd@gmail.com>
2025-11-03 23:18:20 +0800

    Standardise Example Placeholders for Add and Edit

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -2,6 +2,7 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ALLERGY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CHILDNAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PARENTEMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PARENTNAME;
@@ -23,11 +24,12 @@ public class AddCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a child to the address book. "
             + "Parameters: "
-            + PREFIX_CHILDNAME + "NAME "
-            + PREFIX_PARENTNAME + "PARENTNAME "
-            + PREFIX_PARENTPHONE + "PHONE "
-            + PREFIX_PARENTEMAIL + "EMAIL "
+            + PREFIX_CHILDNAME + "CHILD_NAME "
+            + PREFIX_PARENTNAME + "PARENT_NAME "
+            + PREFIX_PARENTPHONE + "PARENT_PHONE_NUMBER "
+            + PREFIX_PARENTEMAIL + "PARENT_EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
+            + "[" + PREFIX_ALLERGY + "ALLERGIES]... "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_CHILDNAME + "John Doe Jr "
@@ -35,6 +37,7 @@ public class AddCommand extends Command {
             + PREFIX_PARENTPHONE + "98765432 "
             + PREFIX_PARENTEMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
+            + PREFIX_ALLERGY + "peanuts "
             + PREFIX_TAG + "friends "
             + PREFIX_TAG + "likes ice cream";
 
